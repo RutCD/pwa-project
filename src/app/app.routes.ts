51ad70bbf89657556da54e29022c5cdf9961f8b4
RutCD <rutchulin0@gmail.com>
2024-02-28 14:20:41 -0500

    feat: ruta foodComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FoodsComponent } from './foods/foods.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component:FoodsComponent
+    }
+];
