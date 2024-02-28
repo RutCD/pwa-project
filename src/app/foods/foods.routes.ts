594b08bbc3477f73eabc28e504edf87c31698ddd
RutCD <rutchulin0@gmail.com>
2024-02-28 18:29:52 -0500

    feat: agregar formfoodComponent

--- src/app/foods/foods.routes.ts
+++ src/app/foods/foods.routes.ts
@@ -1,10 +1,12 @@
 import { Route, Routes } from '@angular/router';
-import { FoodComponent } from './food/food.component';
 import { FoodListComponent } from './food-list/food-list.component';
-
+import { FormFoodComponent } from '../forms/form-food/form-food.component';
 
 export default[
     {
         path: 'food-list', component: FoodListComponent
-    },    
+    },
+    {
+        path: 'form', component: FormFoodComponent
+    }   
 ] satisfies Route[];
