2b9e6bfbb42bf3d4c2d483a97ab4d923ae70b04f
RutCD <rutchulin0@gmail.com>
2024-03-23 02:41:06 -0500

    feat: nuevas rutas

--- src/app/foods/foods.routes.ts
+++ src/app/foods/foods.routes.ts
@@ -1,12 +1,24 @@
 import { Route, Routes } from '@angular/router';
 import { FoodListComponent } from './food-list/food-list.component';
 import { FormFoodComponent } from '../forms/form-food/form-food.component';
+import { DetailsFoodComponent } from './details-food/details-food.component';
+
 
 export default[
     {
-        path: 'food-list', component: FoodListComponent
+        path: 'food-list', component: FoodListComponent,
+        title: 'lista de comida'
     },
     {
-        path: 'form', component: FormFoodComponent
-    }   
+        path: 'form', component: FormFoodComponent,
+        title: 'crear comida'
+    },
+    {
+        path: 'form/:id', component:FormFoodComponent,
+        title: 'Actualizar comida'
+    },
+    {
+        path: 'details-food/:id', component:DetailsFoodComponent,
+        title: 'Comida detalle'
+    }
 ] satisfies Route[];
